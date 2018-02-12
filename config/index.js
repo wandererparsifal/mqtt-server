d03ea7d90e7187f1941128eaf970c5c0a416e636
wandererparsifal <wandererparsifal@gmail.com>
2018-02-12 17:52:32 +0800

    整合服务端浏览器端代码

--- config/index.js
+++ config/index.js
@@ -45,11 +45,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../server/views/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsRoot: path.resolve(__dirname, '../server'),
+    assetsSubDirectory: 'public',
     assetsPublicPath: '/',
 
     /**
